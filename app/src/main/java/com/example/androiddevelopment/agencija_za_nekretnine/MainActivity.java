a5566759cbad34123f1c64e74286f68624a726d4
nikolaJapundzic <nikola_japundzic@outlook.com>
2017-06-24 18:59:42 +0200

    napravljen demo dialog za unos nove nekretnine

--- app/src/main/java/com/example/androiddevelopment/agencija_za_nekretnine/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/agencija_za_nekretnine/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.androiddevelopment.agencija_za_nekretnine;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,11 +48,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_refresh:
-                //refresh();
-                break;
             case R.id.action_add:
-                //addItem();
+                Toast.makeText(this, "Product inserted", Toast.LENGTH_SHORT).show();
+                alert();
                 break;
         }
 
@@ -56,4 +59,29 @@ public class MainActivity extends AppCompatActivity {
 //-------------------- Klikabilnost itema novo ubacenih u toolbar--------------------
 
 //---------------------------------------Tooblbar---------------------------------------------
+
+    public void alert(){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_unos_nove, null);
+        final EditText et_naziv = (EditText) mView.findViewById(R.id.et_naziv);
+        final EditText et_kvadratura = (EditText) mView.findViewById(R.id.et_kvadratura);
+        final EditText et_broj_soba = (EditText) mView.findViewById(R.id.et_broj_soba);
+        final EditText et_broj_telefona = (EditText) mView.findViewById(R.id.et_broj_telefona);
+        final EditText et_adresa = (EditText) mView.findViewById(R.id.et_adresa);
+        final EditText et_cena = (EditText) mView.findViewById(R.id.et_cena);
+        Button btn_save = (Button) mView.findViewById(R.id.btn_save);
+
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
 }
