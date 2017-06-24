db5564bd88560b3fd2fe6a0f4d4b4881d76bebe0
nikolaJapundzic <nikola_japundzic@outlook.com>
2017-06-24 18:29:05 +0200

    toolbar

--- app/src/main/java/com/example/androiddevelopment/agencija_za_nekretnine/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/agencija_za_nekretnine/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.androiddevelopment.agencija_za_nekretnine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //---------------------------------------Tooblbar---------------------------------------------
+
+//-------------------- kreiranje linije--------------------
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle("Nekretnine");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.show();
+        }
     }
+//-------------------- kreiranje linije--------------------
+
+    //-------------------- pozivanje novog .xml itema iz meni foldera konkretno, preuzimanje i nalepljivanje digmica u toolbar--------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_item_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+//-------------------- pozivanje novog .xml itema iz meni foldera konkretno, preuzimanje i nalepljivanje digmica u toolbar--------------------
+
+    //-------------------- Klikabilnost itema novo ubacenih u toolbar--------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                //refresh();
+                break;
+            case R.id.action_add:
+                //addItem();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+//-------------------- Klikabilnost itema novo ubacenih u toolbar--------------------
+
+//---------------------------------------Tooblbar---------------------------------------------
 }
---
+++ app/src/main/java/com/example/androiddevelopment/agencija_za_nekretnine/model/Nekretnina.java
@@ -0,0 +1,8 @@
+package com.example.androiddevelopment.agencija_za_nekretnine.model;
+
+/**
+ * Created by androiddevelopment on 24.6.17..
+ */
+
+public class Nekretnina {
+}
